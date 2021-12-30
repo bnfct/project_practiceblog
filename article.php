c4e571f4e16b25280a4e8f65ceed0059da2687c6
stephenmiller04 <stephenmiller997@gmail.com>
2021-12-30 16:29:05 +0100

    Clickable categoryname viewer page

--- article.php
+++ article.php
@@ -1,7 +1,7 @@
 <?php
 include_once("system/session.php");
 $get_link = $_GET["link"];
-$get_articles = $conn->prepare("SELECT pb_articles.id, pb_articles.author, pb_articles.title, pb_articles.summary, date_format(pb_articles.published, \"%Y-%m-%d %H:%i\") AS published, pb_articles.picture, pb_articles.content, pb_articles.link, pb_users.displayname, pb_categories.title AS categoryname FROM pb_articles INNER JOIN pb_categories ON pb_articles.category = pb_categories.id INNER JOIN pb_users ON pb_articles.author = pb_users.id WHERE pb_articles.hidden=0 AND pb_articles.link=?");
+$get_articles = $conn->prepare("SELECT pb_articles.id, pb_articles.author, pb_articles.title, pb_articles.summary, date_format(pb_articles.published, \"%Y-%m-%d %H:%i\") AS published, pb_articles.picture, pb_articles.content, pb_articles.link, pb_users.displayname, pb_categories.title AS categoryname, pb_categories.link AS categorylink FROM pb_articles INNER JOIN pb_categories ON pb_articles.category = pb_categories.id INNER JOIN pb_users ON pb_articles.author = pb_users.id WHERE pb_articles.hidden=0 AND pb_articles.link=?");
 $get_articles->bind_param("s",$get_link);
 $get_articles->execute();
 $articles_result = $get_articles->get_result();
@@ -33,7 +33,7 @@ $row = $articles_result->fetch_assoc();
                     echo "<p class=\"article-data\">";
                         echo "<span>".$row["published"]."</span>";
                         echo "<span class=\"separator\">::</span>";
-                        echo "<span>".htmlspecialchars($row["categoryname"])."</span>";
+                        echo "<a href=\"/".$row["categorylink"]."\">".htmlspecialchars($row["categoryname"])."</a>";
                         echo "<span class=\"separator\">::</span>";
                         echo "<span>".htmlspecialchars($row["displayname"])."</span>";
                         if(isset($_SESSION["login_user"])) {
--- category.php
+++ category.php
@@ -1,6 +1,25 @@
 <?php
 include_once("system/session.php");
-$get_articles = $conn->prepare("SELECT pb_articles.id, pb_articles.author, pb_articles.title, pb_articles.summary, date_format(pb_articles.published, \"%Y-%m-%d %H:%i\") AS published, pb_articles.picture, pb_articles.content, pb_articles.link, pb_users.displayname, pb_categories.link AS categorylink, pb_categories.title AS categoryname FROM pb_articles INNER JOIN pb_categories ON pb_articles.category = pb_categories.id INNER JOIN pb_users ON pb_articles.author = pb_users.id WHERE pb_articles.hidden=0 ORDER BY pb_articles.id DESC");
+
+$get_category_link = $_GET["link"];
+$get_category_exist = $conn->prepare("SELECT count(id) as countid FROM pb_categories WHERE link=?");
+$get_category_exist->bind_param("s", $get_category_link);
+$get_category_exist->execute();
+$result_category_exist = $get_category_exist->get_result();
+$row_category_exist = $result_category_exist->fetch_assoc();
+
+if ($row_category_exist["countid"] == 0) {
+    header("Location: /");
+}
+$get_category = $conn->prepare("SELECT title, summary FROM pb_categories WHERE link=?");
+$get_category->bind_param("s", $get_category_link);
+$get_category->execute();
+$result_category = $get_category->get_result();
+$row_category = $result_category->fetch_assoc();
+
+
+$get_articles = $conn->prepare("SELECT pb_articles.id, pb_articles.author, pb_articles.title, pb_articles.summary, date_format(pb_articles.published, \"%Y-%m-%d %H:%i\") AS published, pb_articles.picture, pb_articles.content, pb_articles.link, pb_users.displayname, pb_categories.link AS categorylink, pb_categories.title AS categoryname FROM pb_articles INNER JOIN pb_categories ON pb_articles.category = pb_categories.id INNER JOIN pb_users ON pb_articles.author = pb_users.id WHERE pb_articles.hidden=0 AND pb_categories.link=? ORDER BY pb_articles.id DESC");
+$get_articles->bind_param("s", $get_category_link);
 $get_articles->execute();
 $articles_result = $get_articles->get_result();
 ?>
@@ -18,6 +37,10 @@ $articles_result = $get_articles->get_result();
             <?php
                 include_once("components/header.php");
             ?>
+            <div class="category-info-container">
+                <h2><?php echo $row_category["title"];?></h2>
+                <p><?php echo $row_category["summary"];?></p>
+            </div>
             <div class="articles-container">
                 <?php
                     if($articles_result->num_rows>0) {
@@ -46,6 +69,8 @@ $articles_result = $get_articles->get_result();
                                 echo "<p class=\"article-summary\">".htmlspecialchars($row["summary"])."</p></div>"; 
                             echo "</div>";
                         }
+                    } else {
+                        echo "<h2 class=\"no-category-text\">Ebben a kategóriában nincsenek cikkek.</h2>";
                     }
                 ?>    
             </div>
